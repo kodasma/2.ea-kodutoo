656f79f883265620020b9eb63229dc99d74fa974
Kevin Kodasma <31840235+kodasma@users.noreply.github.com>
2018-03-27 22:56:48 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 this.score = 0
 this.level = 1
 this.guessedWords = 0
+
 /* TYPER */
 const TYPER = function () {
   if (TYPER.instance_) {
@@ -8,7 +9,6 @@ const TYPER = function () {
   }
   TYPER.instance_ = this
 
-  this.isReady = false;
   this.routes = TYPER.routes
   this.currentRoute = null
 
@@ -21,32 +21,34 @@ const TYPER = function () {
   this.word = null
   this.wordMinLength = 5
   this.guessedWords = 0
-  this.playerName = "Unknown"
   console.error()
 
-  this.init()
+  this.totalTime = 60;
+  this.timeLeft = this.totalTime
+  this.gameOver = false
+
+  //startGame()
+  //this.init()
 }
 
 window.TYPER = TYPER
 
 TYPER.routes = {
   'home-view': {
-    'render': function() {
+    'render': function () {
       console.log('Home')
     }
   },
   'app-view': {
-    'render': function() {
-      console.log('App')
-
-      
-    }
-  },
-  'score-view': {
-    'render': function() {
+    'render': function () {
       console.log('App')
     }
   }
+  /*'score-view': {
+    'render': function () {
+      console.log('Score')
+    }
+  }*/
 }
 
 TYPER.prototype = {
@@ -59,12 +61,10 @@ TYPER.prototype = {
 
     this.canvas.width = this.WIDTH * 2
     this.canvas.height = this.HEIGHT * 2
-    
+
     this.loadWords()
 
     window.addEventListener('hashchange', this.routeChange.bind(this))
-    var start_button = document.querySelector('#start-button');
-    start_button.addEventListener('click', this.setName.bind(this));
 
     if (!window.location.hash) {
       window.location.hash = 'home-view'
@@ -73,7 +73,7 @@ TYPER.prototype = {
     }
   },
 
-  routeChange: function(event) {
+  routeChange: function (event) {
     this.currentRoute = location.hash.slice(1)
     if (this.routes[this.currentRoute]) {
       this.updateMenu()
@@ -82,7 +82,7 @@ TYPER.prototype = {
     }
   },
 
-  updateMenu: function() {
+  updateMenu: function () {
     document.querySelector('.active-menu').className = document.querySelector('.active-menu').className.replace('active-menu', '')
     document.querySelector('.' + this.currentRoute).className += ' active-menu'
   },
@@ -97,8 +97,7 @@ TYPER.prototype = {
 
         typer.words = structureArrayByWordLength(wordsFromFile)
 
-        //typer.start()
-        typer.isReady = true;
+        typer.start()
       }
     }
 
@@ -112,8 +111,7 @@ TYPER.prototype = {
 
     window.addEventListener('keypress', this.keyPressed.bind(this))
 
-    var name = document.querySelector('#name').value;
-    this.playerName = name;
+    gameTimer()
   },
 
   generateWord: function () {
@@ -126,69 +124,35 @@ TYPER.prototype = {
   },
 
   keyPressed: function (event) {
+    if (typer.gameOver === false) {
+      const letter = String.fromCharCode(event.which)
+      if (letter === this.word.left.charAt(0)) {
+        this.word.removeFirstLetter()
+        score += 1
+        if (this.word.left.length === 0) {
+          guessedWords += 1
+          this.guessedWords += 1
+          this.generateWord()
+        }
+      } else {
+        score -= 5
+      }
+      this.word.Draw()
+    }
+  },
+
+  gameOverButtonPressed: function (event) {
     const letter = String.fromCharCode(event.which)
-		if (letter === this.word.left.charAt(0)) {
-      this.word.removeFirstLetter()
-      score+= 1
-		  if (this.word.left.length === 0) {
-        guessedWords += 1
-        this.guessedWords += 1
-        this.generateWord()
-		  }
-		} else {
-      score-= 5
+    if (letter === " " && typer.gameOver === true) {
+      location.reload()
+      window.location.hash = 'home-view'
+      const typer = new TYPER()
+      window.typer = typer
+      typer.init()
+    } else if (letter === "e" && typer.gameOver === true) {
+      typer.word.drawScores()
     }
-    this.word.Draw()
-  },
-  setName: function(ev) {
-    if (!this.isReady) {
-      console.log("Still loading...");
-      alert("Still loading...");
-      return false;
-    }
-    var name = document.querySelector('#name').value;
-    console.log("Name:", name, ev);
-    //localStorage.setItem('name', name);
-    this.start();
-  },
-  saveScore: function() {
-    var save = {Score: this.score, Name: this.name};
-    var scoreboard = localStorage.getItem("scoreboard");
-    if (scoreboard === undefined) {
-      scoreboard = [];
-    } else {
-      scoreboard = JSON.parse(scoreboard);
-    }
-
-    scoreboard.push(save);
-    localStorage.setItem("scoreboard", JSON.stringify(scoreboard));
-  },
-  showScoreboard: function() {
-    var scoreboard = localStorage.getItem("scoreboard");
-    if (scoreboard === undefined) {
-      return false; //vaata ise
-    }
-    scoreboard = JSON.parse(scoreboard);
-    
-    var makeRow = function (name, score) {
-      var node = document.createElement("div");
-      node.innerHTML = "<div class='scoreboard-name'>" + name+ "</div>" +
-        "<div class='scoreboard-score'>" + score + "</div>";
-      return node;
-    }
-
-    console.log(scoreboard);
-    var node = document.createElement("div");
-    for(var i in scoreboard) {
-      console.log(scoreboard[i]);
-      node.appendChild(makeRow(scoreboard[i].Name, scoreboard[i].Score));
-    }
-    var score_view = document.querySelector('#score-view');
-    score_view.innerHTML = ""; //mida iganes
-    score_view.appendChild(node);
-    //return node;
   }
-
 }
 
 /* WORD */
@@ -206,18 +170,54 @@ Word.prototype = {
     this.ctx.textAlign = 'center'
     this.ctx.font = '140px Courier'
     this.ctx.fillText(this.left, this.canvas.width / 2, this.canvas.height / 2)
-	
-    this.ctx.textAlign = 'left'
-    this.ctx.font = '50px Courier'
-    this.ctx.fillText("Skoor: " + score, 50, 100)
 
     this.ctx.textAlign = 'left'
     this.ctx.font = '50px Courier'
-    this.ctx.fillText("Arvatud sõnad: " + guessedWords, 50, 200)
+    this.ctx.fillText("Aeg: " + typer.timeLeft, 50, 100)
 
     this.ctx.textAlign = 'left'
     this.ctx.font = '50px Courier'
-    this.ctx.fillText("Level: " + level, 50, 300)
+    this.ctx.fillText("Skoor: " + score, 50, 200)
+
+    this.ctx.textAlign = 'left'
+    this.ctx.font = '50px Courier'
+    this.ctx.fillText("Arvatud sõnad: " + guessedWords, 50, 300)
+
+    this.ctx.textAlign = 'left'
+    this.ctx.font = '50px Courier'
+    this.ctx.fillText("Level: " + level, 50, 400)
+  },
+
+  drawEndScreen: function () {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
+    this.ctx.textAlign = 'center'
+    this.ctx.font = '140px Courier'
+    this.ctx.fillText('Aeg läbi!', this.canvas.width / 2, this.canvas.height / 2 - this.canvas.height / 7)
+    this.ctx.font = '80px Courier'
+    this.ctx.fillText('Skoor: ' + score, this.canvas.width / 2, this.canvas.height - this.canvas.height / 2.4)
+    this.ctx.font = '50px Courier'
+    this.ctx.fillText('[E - Edetabel]', this.canvas.width / 2, this.canvas.height - this.canvas.height / 4)
+    this.ctx.fillText('[TÜHIK - Mängi uuesti]', this.canvas.width / 2, this.canvas.height - this.canvas.height / 5)
+
+    window.addEventListener('keypress', TYPER.prototype.gameOverButtonPressed.bind(this))
+  },
+
+  drawScores: function () {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
+    for (let i = 0, len = JSON.parse(localStorage.getItem('arr')).length; i < len; i++) {
+      if (i < 10) {
+        this.ctx.textAlign = 'center'
+        this.ctx.font = '80px Courier'
+        this.ctx.fillText("#" + (i + 1) + ") " +
+          "Nimi: " + JSON.parse(localStorage.getItem('arr'))[i][0] + "    " +
+          "Skoor: " + JSON.parse(localStorage.getItem('arr'))[i][1],
+          this.canvas.width / 2, 200 + 100 * i)
+      }
+    }
+    this.ctx.font = '50px Courier'
+    this.ctx.fillText('[TÜHIK - Mängi uuesti]', this.canvas.width / 2, this.canvas.height - this.canvas.height / 10)
   },
 
   removeFirstLetter: function () {
@@ -226,12 +226,12 @@ Word.prototype = {
 }
 
 /* HELPERS */
-function structureArrayByWordLength (words) {
+function structureArrayByWordLength(words) {
   let tempArray = []
 
   for (let i = 0; i < words.length; i++) {
     const wordLength = words[i].length
-    if (tempArray[wordLength] === undefined)tempArray[wordLength] = []
+    if (tempArray[wordLength] === undefined) tempArray[wordLength] = []
 
     tempArray[wordLength].push(words[i])
   }
@@ -239,7 +239,61 @@ function structureArrayByWordLength (words) {
   return tempArray
 }
 
-window.onload = function () {
-  const typer = new TYPER()
-  window.typer = typer
+function gameTimer() {
+  (function timer1() {
+    if (typer.timeLeft > 0) {
+      typer.word.Draw()
+      typer.timeLeft -= 1
+      setTimeout(timer1, 1000)
+    } else {
+      endGame()
+    }
+  })();
+}
+
+function endGame() {
+  typer.gameOver = true
+  typer.word.drawEndScreen()
+  saveScore(name, this.score)
+}
+
+function saveScore(playerName, playerScore) {
+  arr = []
+  stored = []
+  if (window.localStorage.length == 0) {
+    player = [playerName, playerScore]
+    arr.push(player)
+    localStorage.setItem('arr', JSON.stringify(arr))
+  } else {
+    let player2 = [playerName, playerScore]
+    stored.push(player2)
+
+
+    let length = stored.length
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < (length - i - 1); j++) {
+        if (stored[j][1] < stored[j + 1][1]) {
+          let tmp = stored[j]
+          stored[j] = stored[j + 1]
+          stored[j + 1] = tmp
+        }
+      }
+    }
+    localStorage.setItem('arr', JSON.stringify(stored))
+  }
+}
+
+let name = ""
+
+function startGame() {
+  if (document.querySelector('#nameField').value != "") {
+    name = document.querySelector('#nameField').value
+    document.querySelector('#app-view').innerHTML = "<canvas></canvas>"
+    window.location.hash = 'app-view'
+    const typer = new TYPER()
+    window.typer = typer
+    typer.init()
+  } else {
+    alert("Kõigepealt sisesta nimi!")
+  }
 }
